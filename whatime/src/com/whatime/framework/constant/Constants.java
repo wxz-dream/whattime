1f31e69b1f32191ae26aa9862e955347fafee77c
devilangel <devilangel@2263f374-173e-4805-a94b-0f7599168a5e>
2014-04-01 10:05:20 +0000

    6

--- whatime/src/com/whatime/framework/constant/Constants.java
+++ whatime/src/com/whatime/framework/constant/Constants.java
@@ -25,6 +25,8 @@ public class Constants
     
     public static final String MODIFY_USER_POST_URL = "user/uptUserInfo";
     
+    public static final String GET_USER_POST_URL = "user/getUser";
+    
     public static final String ALARM_LOCAL_ADD_POST_URL = "userLocalAlarm/alarmLocalAdd";
     
     public static final String ALARM_SHARE_ADD_POST_URL = "userShareAlarm/alarmShareAdd";
