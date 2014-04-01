586642910844a75c6f159567e04c090561fbc9e4
devilangel <devilangel@2263f374-173e-4805-a94b-0f7599168a5e>
2014-04-01 10:04:31 +0000

    3

--- whatime/src/com/whatime/framework/ui/fragment/BaseFragment.java
+++ whatime/src/com/whatime/framework/ui/fragment/BaseFragment.java
@@ -12,7 +12,6 @@ import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.whatime.framework.ui.activity.MainActivity;
